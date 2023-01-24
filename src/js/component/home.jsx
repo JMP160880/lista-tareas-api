7313a48bcf6ebb62c278287277b89429e17ba471
JMP160880 <javiermoranpando@gmail.com>
2023-01-24 17:09:29 +0000

    comienzo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,44 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+
+
+
+
+
+	
+	const [input, setInput] = useState("");
+	const [array, setArray] = useState([]);
+
+	const addTask = (e) =>{
+		if(e.keyCode === 13){
+			setArray(array.concat(input));
+			setInput("")
+		}
+	}
+
+	const deleteTask =(index) =>{
+		let borrar = array[index]
+		setArray(array.filter((item) => item!=borrar))
+	}
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1>LISTA DE TAREAS PENDIENTES</h1>
+			<ul>
+				<li>
+					<input onChange={(e)=>setInput(e.target.value)} value={input}
+					 placeholder=" - AÑADIR TAREA" 
+					 onKeyDown={addTask}></input>
+				</li>
+				{array.map((item,index) => (
+					<li  key={index}>
+						{item} <button onClick={()=>deleteTask(index)}>
+						<i class="fa fa-trash"></i></button>
+					</li>
+				))}
+			</ul>
+			<div id="total"> {array.length}  TAREAS POR REALIZAR </div>
 		</div>
 	);
 };
