0f2731b4834b4636b0023e2b8c2a6564ebcee6f2
JMP160880 <javiermoranpando@gmail.com>
2023-03-05 19:07:24 +0000

    terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
 
 	const addTask = (e) =>{
 		if(e.keyCode === 13){
-			setArray(array.concat({label: input, done: false}));
-			setInput("")
+			setArray([...array,{label: input, done: false}]);
+			setInput("");
 		}
 	}
 
@@ -17,68 +17,78 @@ const Home = () => {
 		setArray(array.filter((item) => item!=borrar))
 	}
 
-	const logarUsario = () =>{
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/javier', {
-      method: "POST",
-      body:JSON.stringify([]),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then((response) =>response.json())
-    .then((data) =>console.log(data))
-    .catch((error) =>console.log(error))     
-	}
-
-	const conectarLista = () =>{
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/javier', {
-      method: "GET",
-    })
-    .then((response) =>response.json())
-    .then((data) =>setArray(array.concat({label: input, done: false})))
-    .catch((error) =>console.log(error))     
-	}
-	const agregarTarea = () =>{
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/javier', {
-      method: "PUT",
-      body:JSON.stringify(array),
-      headers: {
-        "Content-Type": "application/json"
-      }
-	  
-    })
-    .then((response) =>response.json())
-    .then((data) =>console.log(data))
-    .catch((error) =>console.log(error))     
-	}
-
-	const eliminarTarea = () =>{
-		fetch('https://assets.breatheco.de/apis/fake/todos/user/javier', {
-      method: "PUT",
-      body:JSON.stringify([]),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
-    .then((response) =>response.json())
-    .then((data) =>console.log(data))
-    .catch((error) =>console.log(error))     
-	}
-	useEffect(()=>{
-		logarUsario()
-	},[])
-	useEffect(()=>{
-		conectarLista()
-	},[])
-	useEffect(()=>{
-		agregarTarea()
-	},[addTask])
-	useEffect(()=>{
-		eliminarTarea()
-	},[deleteTask])
-
+	function createUser() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/javier", {
+		  method: "POST",
+		  headers: {
+			"Content-Type": "application/json",
+		  },
+		  body: JSON.stringify([]), 
+		}) 
+		  .then((response) => response.json()) 
+		  .then((data) => {
+			
+		  if (data.result === "ok") {getUser()}
+	
+			console.log(data)}) 
+		  .catch((error) => console.log(error)); 
+	  }
+	
+	function getUser() {
+		fetch("https://assets.breatheco.de/apis/fake/todos/user/javier", {
+		  method: "GET",
+		}) 
+		  .then((response) => {
+		   console.log(response.status); 
+	
+		  if (response.status === 404) {createUser()} 
+	
+			
+			return response.json()}) 
+		  .then((data) => setArray(data)) 
+		  .catch((error) => console.log(error)); 
+	  }
+	
+	function updateToDos() {
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/javier',{
+			method: 'PUT', 
+			headers: {
+				   'Content-Type': 'application/json'
+		   },
+	
+		    body: JSON.stringify(array)
+	
+		  })
+		  .then((response)=>response.json())
+		  .then((data)=>console.log(data))
+		  .catch((error)=>console.log(error))
+	  }
 	
 	
+	function deleteToDos() {
+		fetch('https://assets.breatheco.de/apis/fake/todos/user/javier',{
+		  	method: 'DELETE', 
+	   })
+		   .then((response)=>response.json())
+		   .then((data) => console.log(data))
+		   .catch((error)=>console.log(error))
+	   }
+	
+	  useEffect(() => {
+		
+		getUser();
+
+		console.log("Ejecutando, componente cargado");
+
+	  }, []); 
+	
+	  useEffect(() => {
+		
+		if (array.length > 0) {
+		   updateToDos();
+		}
+		  
+	  }, [array]); 
 	
 	return (
 		<div className="container-sm">
@@ -97,7 +107,11 @@ const Home = () => {
 				)): null}
 			</ul>
 			<div id="total">{array.length}  TAREAS POR REALIZAR </div>
+			<div className="grid text-center my-2">
+        	<button className="col-2" onClick={() => deleteToDos()}>LIMPIAR DATOS</button>
+          	</div>
 		</div>
+		
 	);
 };
 
